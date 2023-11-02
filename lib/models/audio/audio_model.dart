31ac344750ca89cc79fe26d64f7ce2a38467dd00
Yilmaz Yagiz Dokumaci <dokumaciyagiz@gmail.com>
2023-11-02 12:38:20 +0300

    Added audio model

---
+++ lib/models/audio/audio_model.dart
@@ -0,0 +1,15 @@
+class AudioModel {
+  String id;
+  String url;
+  String title;
+  String artUri;
+  String artist;
+
+  AudioModel({
+    required this.id,
+    required this.url,
+    required this.title,
+    required this.artUri,
+    required this.artist,
+  });
+}
